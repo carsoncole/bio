a70ce10ef72faf71e74a061514a1d070b96152b1
Carson Cole <carson.cole@protonmail.com>
2021-12-07 17:19:38 -0800

    link added to experiences

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :signed_in
+
+  def signed_in
+    @signed_in ||= ActionController::HttpAuthentication::Basic.has_basic_credentials?(request)
+  end
 end
