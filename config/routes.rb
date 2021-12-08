8f9a8c6ffb8cb5a175b81c69b5c0c1a8577be019
Carson Cole <carson.cole@protonmail.com>
2021-12-08 09:17:00 -0800

    minor

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  resources :settings
   root "home#index"
 
   resources :experiences
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :settings
 end
