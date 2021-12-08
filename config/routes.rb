98963db8de6c07f5e0fe68a62918c5268085a92c
Carson Cole <carson.cole@protonmail.com>
2021-12-08 12:06:51 -0800

    removed login link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "home#index"
 
+  get '/login' => 'experiences#new'
+
   resources :experiences, only: [:edit, :new, :update, :create, :destroy]
   resources :settings
 end
